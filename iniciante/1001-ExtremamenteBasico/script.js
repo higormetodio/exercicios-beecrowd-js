2fe62550e02dc5bcb503dc391650ffa06539e876
Higor Metodio <higor.metodio@outlook.com>
2023-12-26 19:58:00 -0300

    solucao problema 1001-Extremamente Basico

---
+++ iniciante/1001-ExtremamenteBasico/script.js
@@ -0,0 +1,8 @@
+//https://www.beecrowd.com.br/judge/pt/problems/view/1001
+
+var variavelA = parseInt(lines.shift());
+var variavelB = parseInt(lines.shift());
+
+var variavelX = variavelA + variavelB;
+
+console.log("X = " + variavelX);
